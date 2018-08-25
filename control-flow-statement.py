f546011b7b6d7f62a66cfd8e1a024dc2166b0ed4
Raj Ranjan <raj.ranjan@alepo.com>
2018-08-25 08:56:28 +0000

    variables and control flow

---
+++ control-flow-statement.py
@@ -0,0 +1,29 @@
+# gift distribution scenario
+
+"""
+
+    age greater than 15 years give 15 choclate
+    age greater than 10 and less than equal to 15 give 10 choclate
+    age greater than 5 years and less than equal to 10 years give 5 choclates
+    age less than 5 years no choclate
+
+"""
+age = 2
+
+if age > 15 :
+
+    print("You will get 15 choclate")
+
+elif age > 10 and age <= 15:
+
+    print("You will get 10 choclate")
+
+elif age > 5 and age <=10 : 
+
+    print("you will get 5 choclate")
+
+else : 
+
+    print("you will get no choclate")
+
+
---
+++ variables.py
@@ -0,0 +1,37 @@
+# integer inside variable in python
+
+number = 10
+print(number)
+print(type(number))
+
+
+# string inside variable in python
+
+name = "Selftuts"
+print(name)
+print(type(name))
+
+
+# float inside variable in python
+
+data = 10.5
+print(data)
+print(type(data))
+
+
+# boolean inside variable in python
+
+option1 = True
+print(option1)
+print(type(option1))
+
+
+option2 = True
+print(option2)
+print(type(option2))
+
+
+
+
+
+
