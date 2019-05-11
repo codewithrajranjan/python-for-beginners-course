fb97754894c363e29605413b5afbfe576d601ca3
Raj Ranjan <raj.ranjan@alepo.com>
2019-05-11 03:02:20 +0000

    changs

--- control-flow-statement.py
+++ control-flow-statement.py
@@ -2,9 +2,9 @@
 
 """
 
-    age greater than 15 years give 15 choclate
-    age greater than 10 and less than equal to 15 give 10 choclate
-    age greater than 5 years and less than equal to 10 years give 5 choclates
+    age greater than 15 years give 15 chocolate
+    age greater than 10 and less than equal to 15 give 10 chocolate
+    age greater than 5 years and less than equal to 10 years give 5 chocolates
     age less than 5 years no choclate
 
 """
@@ -12,18 +12,18 @@ age = 2
 
 if age > 15 :
 
-    print("You will get 15 choclate")
+    print("You will get 15 chocolate")
 
 elif age > 10 and age <= 15:
 
-    print("You will get 10 choclate")
+    print("You will get 10 chocolate")
 
 elif age > 5 and age <=10 : 
 
-    print("you will get 5 choclate")
+    print("you will get 5 chocolate")
 
 else : 
 
-    print("you will get no choclate")
+    print("you will get no chocolate")
 
 
